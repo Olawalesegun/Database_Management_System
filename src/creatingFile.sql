b1392a05949b18055968bdece31964d0506f13ee
Olawalesegun <olawalesegun0@gmail.com>
2023-08-26 23:14:08 +0100

    Hands on learning database management system

---
+++ src/creatingFile.sql
@@ -0,0 +1,8 @@
+/*create database dudu;
+
+CREATE TABLE dudu.cities(
+                            name VARCHAR(50),
+                            country VARCHAR(50),
+                            population INTEGER,
+                            area INTEGER
+);*/
---
+++ src/introToDatabaseMgt.sql
@@ -0,0 +1,5 @@
+/*Data Design Process
+     The questions to ask when designing the data process includes:
+     1. What kind of thing am I going to store
+     2. What kind of properties (can also be referred to as attributes) does this thing that we intend to store have
+     3. What type of data does each of these properties contain*/
