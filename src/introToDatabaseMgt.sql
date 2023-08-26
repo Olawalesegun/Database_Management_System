5b9987f933ab7a793881d2cbadbcb8d6da8a085a
Olawalesegun <olawalesegun0@gmail.com>
2023-08-26 23:19:02 +0100

    data design proces

--- src/introToDatabaseMgt.sql
+++ src/introToDatabaseMgt.sql
@@ -1,5 +1,5 @@
-/*Data Design Process
+Data Design Process
      The questions to ask when designing the data process includes:
      1. What kind of thing am I going to store
      2. What kind of properties (can also be referred to as attributes) does this thing that we intend to store have
-     3. What type of data does each of these properties contain*/
+     3. What type of data does each of these properties contain
