e5f9a389b54a89589ff49639fae227eb26c1924f
Olawalesegun <olawalesegun0@gmail.com>
2023-08-26 23:18:20 +0100

    creating file in sql

--- src/creatingFile.sql
+++ src/creatingFile.sql
@@ -1,8 +1,8 @@
-/*create database dudu;
+create database dudu;
 
 CREATE TABLE dudu.cities(
                             name VARCHAR(50),
                             country VARCHAR(50),
                             population INTEGER,
                             area INTEGER
-);*/
+);
